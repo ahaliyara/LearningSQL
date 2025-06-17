f2ca73c20e9bddcc40d2c4bbbcf89db7c193840b
ahaliyara <ahaliyara@gmail.com>
2025-06-16 20:45:21 -0700

    Update Foodr.sql
    Calculating cost

--- Foodr.sql
+++ Foodr.sql
@@ -7,3 +7,13 @@ SELECT
 FROM meals  
 JOIN orders ON meals.meal_id = orders.meal_id  
 GROUP BY order_id; 
+
+SELECT  
+  meals.meal_id,   
+  SUM(meal_cost * stocked_quantity) AS cost   
+FROM meals  
+JOIN stock ON meals.meal_id = stock.meal_id  
+GROUP BY meals.meal_id  
+ORDER BY cost DESC  
+LIMIT 3;
+
