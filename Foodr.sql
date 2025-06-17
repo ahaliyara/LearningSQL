34fe1c9f05255f336c3a0ad0eaace2f45c7fe741
ahaliyara <ahaliyara@gmail.com>
2025-06-16 20:50:40 -0700

    Calculating Profit
    . Bringing Revenue and Cost Together

--- Foodr.sql
+++ Foodr.sql
@@ -47,3 +47,30 @@ FROM costs_and_quantities
 ORDER BY cost DESC  
 LIMIT 3;
 
+WITH revenue AS (  
+  SELECT  
+    meals.meal_id,  
+    SUM(meal_price * order_quantity) AS revenue  
+  FROM meals  
+  JOIN orders ON meals.meal_id = orders.meal_id  
+  GROUP BY meals.meal_id 
+),   
+cost AS (  
+  SELECT  
+    meals.meal_id,  
+    SUM(meal_cost * stocked_quantity) AS cost  
+  FROM meals  
+  JOIN stock ON meals.meal_id = stock.meal_id  
+  GROUP BY meals.meal_id 
+)
+SELECT  
+  revenue.meal_id,  
+  revenue,  
+  cost,  
+  revenue - cost AS profit  
+FROM revenue  
+JOIN cost ON revenue.meal_id = cost.meal_id  
+ORDER BY profit DESC  
+LIMIT 3; 
+
+
