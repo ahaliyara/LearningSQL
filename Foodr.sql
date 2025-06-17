a9d7b907878b6e6360d92418d61f67079fa1cf1a
ahaliyara <ahaliyara@gmail.com>
2025-06-16 20:42:46 -0700

    Update Foodr.sql
    Finding the Revenue

--- Foodr.sql
+++ Foodr.sql
@@ -1,2 +1,9 @@
 SELECT * FROM public.meals
 LIMIT 100
+
+SELECT  
+  order_id,  
+  SUM(meal_price * order_quantity) AS revenue  
+FROM meals  
+JOIN orders ON meals.meal_id = orders.meal_id  
+GROUP BY order_id; 
