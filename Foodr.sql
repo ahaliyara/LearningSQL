d0bb32057731dd3a333f1f35abb660dafe3b9132
ahaliyara <ahaliyara@gmail.com>
2025-07-03 21:59:46 -0700

    Update Foodr.sql

--- Foodr.sql
+++ Foodr.sql
@@ -82,4 +82,20 @@ GROUP BY user_id
 ORDER BY user_id 
 LIMIT 3;
 
+--Registrations - query
+WITH reg_dates AS ( 
+  SELECT 
+    user_id, 
+    MIN(order_date) AS reg_date 
+  FROM orders 
+  GROUP BY user_id) 
+SELECT 
+  DATE_TRUNC('month', reg_date) :: DATE AS foodr_month, 
+  COUNT(DISTINCT user_id) AS regs 
+FROM reg_dates 
+GROUP BY foodr_month 
+ORDER BY foodr_month ASC 
+LIMIT 3;
+
+
 
