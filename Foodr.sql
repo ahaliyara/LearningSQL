f96a11b9bb87d28be3898ac99dc13e8170e5c22d
ahaliyara <ahaliyara@gmail.com>
2025-07-03 22:01:58 -0700

    Update Foodr.sql

--- Foodr.sql
+++ Foodr.sql
@@ -127,6 +127,24 @@ FROM registrations
 ORDER BY foodr_month ASC 
 LIMIT 3;
 
+---Lagged MAU - query
+WITH maus AS ( 
+  SELECT 
+    DATE_TRUNC('month', order_date) :: DATE AS foodr_month, 
+    COUNT(DISTINCT user_id) AS mau 
+  FROM orders 
+  GROUP BY foodr_month)  
+ SELECT 
+  foodr_month, 
+  mau, 
+  COALESCE( 
+    LAG(mau) OVER (ORDER BY foodr_month ASC), 
+  1) AS last_mau 
+FROM maus 
+ORDER BY foodr_month ASC 
+LIMIT 3;
+
+
 
 
 
