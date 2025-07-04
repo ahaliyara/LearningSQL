a74738c9c2994c2219c1c37af566431f64a3483c
ahaliyara <ahaliyara@gmail.com>
2025-07-03 22:02:44 -0700

    Update Foodr.sql

--- Foodr.sql
+++ Foodr.sql
@@ -145,6 +145,31 @@ ORDER BY foodr_month ASC
 LIMIT 3;
 
 
+---Deltas - query
+WITH maus AS ( 
+  SELECT 
+    DATE_TRUNC('month', order_date) :: DATE AS foodr_month, 
+    COUNT(DISTINCT user_id) AS mau 
+  FROM orders 
+  GROUP BY foodr_month),  
+  maus_lag AS ( 
+  SELECT 
+    foodr_month, 
+    mau, 
+    COALESCE( 
+      LAG(mau) OVER (ORDER BY foodr_month ASC), 
+    1) AS last_mau 
+  FROM maus) 
+SELECT 
+  foodr_month, 
+  mau, 
+  mau - last_mau AS mau_delta 
+FROM maus_lag 
+ORDER BY foodr_month 
+LIMIT 3;
+
+
+
 
 
 
